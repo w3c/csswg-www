526cef2ab6f401912456c0b8bd6324d786839421
Mihir Kedia <meitros@gmail.com>
2022-12-04 13:15:44 -0500

    Fix assets in 11ty

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,20 @@
-module.exports = function(eleventyConfig) {
-
+module.exports = function (eleventyConfig) {
     // Custom Markdown config
     let markdownIt = require("markdown-it");
     let options = {
-        html: true
+        html: true,
     };
-    eleventyConfig.setLibrary("md", markdownIt(options).disable('code'));
+    eleventyConfig.setLibrary("md", markdownIt(options).disable("code"));
+
+    eleventyConfig.addPassthroughCopy("_content/assets");
 
     return {
         dir: {
             input: "_content",
             includes: "../_includes",
             layouts: "../_layouts",
-            data: "../_data"
+            data: "../_data",
         },
-        templateFormats: [
-            "md",
-            "css",
-            "js"
-        ]
+        templateFormats: ["md"],
     };
-
 };
